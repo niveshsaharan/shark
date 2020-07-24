1d29ab4263148f68b96603d470d1f88986c3f11a
Nivesh Saharan <nivesh@gmail.com>
2020-07-24 22:07:25 +0530

    Updates README.md, adds github actions, adds test for home route

---
+++ CHANGELOG.md
@@ -0,0 +1,4 @@
+# Changelog
+
+All notable changes to `shark` will be documented in this file
+
---
+++ tests/Feature/RoutesTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+
+/**
+ * Class RoutesTest
+ *
+ * @package \Tests\Feature
+ */
+class RoutesTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function it_has_home_route_and_uses_correct_middlewares()
+    {
+        $this->assertRouteUsesMiddleware('home', ['web', 'auth.shopify'], true);
+    }
+}
