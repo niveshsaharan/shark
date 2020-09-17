27f5f1eb17b79fbb0768edaeadecfe736fff675f
niveshsaharan <niveshsaharan@users.noreply.github.com>
2020-09-17 10:07:05 +0000

    Fix styling

--- app/Models/User.php
+++ app/Models/User.php
@@ -20,7 +20,6 @@ class User extends Authenticatable implements IShopModel
         HasApiTokens,
         HasSettingsTable,
         HasFactory;
-
     use ShopModel {
         apiHelper as originalApiHelper;
     }
