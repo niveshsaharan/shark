6a6b9279861d22485e0f315321ab03431dc02090
Nivesh Saharan <niveshsaharan@gmail.com>
2022-06-19 15:13:51 +0530

    Add special offer

---
+++ app/Jobs/FetchSpecialOffers.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class FetchSpecialOffers implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * The shop domain.
+     *
+     * @var \App\Models\User
+     */
+    protected $shop;
+
+    /**
+     * Create a new job instance.
+     *
+     * @param \App\Models\User $shop  The shop.
+     * @return void
+     */
+    public function __construct($shop)
+    {
+        $this->shop = $shop;
+    }
+
+    /**
+     * Execute the job
+     */
+    public function handle()
+    {
+        return $this->shop->updateFromGraphApiResponse(
+            $this->shop->api()->graph('{
+                shop {
+                    id
+                    name
+                    email
+                    contactEmail
+                    currencyCode
+                    currencyFormats {
+                        moneyFormat
+                        moneyInEmailsFormat
+                        moneyWithCurrencyFormat
+                        moneyWithCurrencyInEmailsFormat
+                    }
+                    primaryDomain {
+                        host
+                    }
+                    timezoneOffset
+                    ianaTimezone
+                    enabledPresentmentCurrencies
+                    plan {
+                        displayName
+                        partnerDevelopment
+                        shopifyPlus
+                    }
+                    billingAddress {
+                        city
+                        province
+                        provinceCode
+                        country
+                        countryCodeV2
+                    }
+                }
+            }')['body']['data']['shop']
+        );
+    }
+}
