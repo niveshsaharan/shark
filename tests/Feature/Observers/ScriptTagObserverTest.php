aee62b617c5c3d687d75439bc8edb0014a256ee7
Nivesh Saharan <niveshsaharan@gmail.com>
2022-09-12 10:27:06 +0530

    Script tag tests

---
+++ tests/Feature/Observers/ScriptTagObserverTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Tests\Feature\Observers;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Hash;
+use Tests\TestCase;
+
+class ScriptTagObserverTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * @test
+     */
+    public function creating()
+    {
+        $user = User::factory()->create([
+            'name' => 'somename.myshopify.com',
+            'email' => '',
+            'password' => null,
+        ]);
+
+        $user2 = User::factory()->create([
+            'name' => 'somename.myshopify.com',
+            'email' => 'shop@someothervalue.com',
+            'password' => Hash::make('password'),
+        ]);
+
+        $this->assertEquals('shop@somename.myshopify.com', $user->email);
+        $this->assertEquals('', $user->password);
+
+        $this->assertEquals('shop@someothervalue.com', $user2->email);
+        $this->assertTrue(Hash::check('password', $user2->password));
+    }
+}
