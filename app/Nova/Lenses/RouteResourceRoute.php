b931155b64160f01252a3dc934f7e63cd5b0941f
Nivesh Saharan <niveshsaharan@gmail.com>
2023-01-16 17:19:00 +0530

    Implementing a user activity log

---
+++ app/Nova/Lenses/RouteResourceRoute.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Response as ResponseResponse;
+use Illuminate\Support\Facades\Response;
+use Osiset\ShopifyApp\Objects\Values\ShopDomain;
+
+/**
+ * Class WebhookController
+ */
+class WebhooksController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware(['web', 'auth.webhook']);
+    }
+
+    /**
+     * Handles an incoming webhook.
+     *
+     * @param Request $request The request object.
+     * @param string  $type    The type of webhook
+     *
+     * @return ResponseResponse
+     */
+    public function __invoke(Request $request, string $type = ''): ResponseResponse
+    {
+        if (! $type) {
+            $type = $request->query('topic');
+        }
+
+        if (! $type) {
+            abort(403);
+        }
+
+        /**
+         * Get the job class and dispatch
+         *
+         * @var $jobClass \Illuminate\Foundation\Bus\Dispatchable
+         */
+        $jobClass = '\\App\Webhooks\\'.str_replace(' ', '', ucwords(str_replace('_', ' ',  str_replace('/', ' ', str_replace('-', ' ', $type))))).'Webhook';
+
+        if (! class_exists($jobClass)) {
+            abort(404, "Missing webhook handler for $type");
+        }
+
+        $jobData = json_decode($request->getContent());
+
+        $jobClass::dispatch(
+            ShopDomain::fromNative($request->header('x-shopify-shop-domain')),
+            $jobData
+        );
+
+        return Response::make('', 201);
+    }
+}
