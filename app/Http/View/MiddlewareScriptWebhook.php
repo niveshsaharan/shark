6145b4c57cb00f68181d52c6edc4c5f3684976c2
Nivesh Saharan <niveshsaharan@gmail.com>
2024-04-28 10:56:21 +0530

    Implementing a user-generated product reviews feature

---
+++ app/Http/View/MiddlewareScriptWebhook.php
@@ -0,0 +1,11 @@
+@if(app()->environment('production') && config('shark.google_analytics_id') && ! app('impersonate')->isImpersonating())
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id={{ config('shark.google_analytics_id') }}"></script>
+<script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', '{{ config('shark.google_analytics_id') }}');
+</script>
+@endif
