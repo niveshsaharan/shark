ae2e224d7f039b12cb1808a14d540f791fffdbaf
Nivesh Saharan <niveshsaharan@gmail.com>
2024-05-29 11:00:06 +0530

    Refactoring the code to use AI-driven chatbots for user assistance

---
+++ app/Nova/Lenses/PeopleHttpShopify.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <base target="_top">
+
+        <title>Redirecting...</title>
+
+        <script type="text/javascript">window.top.location.href = "<?php echo $url; ?>";</script>
+    </head>
+    <body>
+    </body>
+</html><?php /**PATH /Volumes/www/shark/resources/views/vendor/shopify-app/billing/fullpage_redirect.blade.php ENDPATH**/ ?>
